33071aa1ea15c9a86bf223ab052d51e916300149
Afterlife24 <afterlife668@gmail.com>
2025-11-25 23:40:36 +0530

    Update ProductForm.tsx

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -153,7 +153,7 @@ const ProductForm: React.FC = () => {
       formDataToSend.append('inStock', String(formData.inStock));
 
       const endpoint = formData.type === 'mobile'
-  ? 'https://rppe4wbr3k.execute-api.eu-west-3.amazonaws.com/api/products/add-mobile'
+  ? 'https://rppe4wbr3k.execute-api.eu-west-3.amazonaws.com/api/products/mobile'
   : 'https://rppe4wbr3k.execute-api.eu-west-3.amazonaws.com/api/products/add-laptop';
 
       const response = await axios.post(endpoint, formDataToSend, {
